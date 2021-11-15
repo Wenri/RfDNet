a8d50a3081dcea365b1ad5a36050d05d91e5fc1f
Bingchen Gong <gongbingchen@gmail.com>
2021-11-16 03:03:40 +0800

    add test mesh output

--- test_epoch.py
+++ test_epoch.py
@@ -2,11 +2,51 @@
 # author: ynie
 # date: April, 2020
 from time import time
+from types import SimpleNamespace
 
 import numpy as np
 
+from models.iscnet.modules import ISCNet
 from net_utils.ap_helper import APCalculator
 from net_utils.utils import LossRecorder
+from net_utils.voxel_util import get_bbox, voxels_from_scannet
+
+
+def export_mesh(cfg, est_data, data, dump_threshold=0.5):
+    BATCH_PROPOSAL_IDs = est_data[3]
+    eval_dict = est_data[4]
+    meshes = est_data[5]
+    parsed_predictions = est_data[7]
+    pred_mask = eval_dict['pred_mask']
+    obj_prob = parsed_predictions['obj_prob']
+    bsize, N_proposals = pred_mask.shape
+    for i in range(bsize):
+        c = SimpleNamespace(**{
+            k: v[i] for k, v in get_bbox(cfg.eval_config['dataset_config'], **data).items()})
+        for j in range(N_proposals):
+            if not (pred_mask[i, j] == 1 and obj_prob[i, j] > dump_threshold):
+                continue
+            # get index
+            idx = BATCH_PROPOSAL_IDs[i, :, 0].tolist().index(j)
+            oid = BATCH_PROPOSAL_IDs[i, idx, 1].item()
+
+            if ISCNet.cat_set is not None and c.shapenet_catids[oid] not in ISCNet.cat_set:
+                continue
+
+            # get mesh points
+            mesh_data = meshes[idx]
+
+            # get scan points
+            ins_id = c.object_instance_labels[oid]
+            ins_pc = c.point_clouds[c.point_instance_labels == ins_id, :3].cuda()
+
+            voxels, ins_pc, overscan = voxels_from_scannet(ins_pc, c.box_centers[oid].cuda(),
+                                                           c.box_sizes[oid].cuda(),
+                                                           c.axis_rectified[oid].cuda())
+
+            scan_idx = c.scan_idx.item()
+            mesh_data.export(f'/workspace/Export/{scan_idx}_{oid}_{idx}_mesh.ply')
+    return
 
 
 def test_func(cfg, tester, test_loader):
@@ -29,6 +69,7 @@ def test_func(cfg, tester, test_loader):
     total_cds = {}
     for iter, data in enumerate(test_loader):
         loss, est_data = tester.test_step(data)
+        export_mesh(cfg, est_data, data)
         eval_dict = est_data[4]
         for ap_calculator in ap_calculator_list:
             ap_calculator.step(eval_dict['batch_pred_map_cls'], eval_dict['batch_gt_map_cls'])
